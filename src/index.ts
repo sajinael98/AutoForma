af1b2f99d7bb1fca0d3bdb9e384e3f1cb9853231
sajinael98 <saji.nael.98@gmail.com>
2025-10-16 22:42:37 -0500

    feat(index): export FieldRendererResolver and FieldLayoutWrapper from library entry point

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 // src/index.ts
 export { default as AutoForm } from "@/components/AutoForm/AutoForm";
 export type { AutoFormProps } from "@/components/AutoForm/AutoForm.types";
-export type { FieldRendererProps } from "@/fields/renderer.types";
+export type { FieldRendererProps } from "@/fields/FieldRenderer/FieldRenderer.types";
 export type { FieldSchema } from "@/fields/types";
+export { FieldRendererResolver } from "@/fields/resolver/FieldRendererResolver";
+export { default as FieldLayoutWrapper } from "@/fields/FieldRenderer/FieldLayoutWrapper";
